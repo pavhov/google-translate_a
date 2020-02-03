d7682d0e468dc8756503858988e27d110705bb50
pavhov <8784893+pavhov@users.noreply.github.com>
2020-02-04 00:25:20 +0400

    Update translate_a.js

--- translate_a.js
+++ translate_a.js
@@ -37,6 +37,27 @@ var translate_a = (function () {
                     })
             });
 
+            var settings = {
+              "url": "https://translate.google.com/translate_a/single?client=gtx&sl=ru&tl=hy&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&otf=2&ssel=0&tsel=0&xid=1788074&kc=1&tk=798373.695705&q=test",
+              "method": "GET",
+              "timeout": 0,
+              "headers": {
+                "authority": "translate.google.com",
+                "pragma": "no-cache",
+                "cache-control": "no-cache",
+                "dnt": "1",
+                "accept": "*/*",
+                "sec-fetch-site": "same-origin",
+                "sec-fetch-mode": "cors",
+                "referer": "https://translate.google.com/",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,hy;q=0.6,it;q=0.5"
+              },
+            };
+
+            $.ajax(settings).done(function (response) {
+              console.log(response);
+            });
         }
     };
 })();
